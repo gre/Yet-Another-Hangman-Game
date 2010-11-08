b2a160fc0d2a99f81ff0c4b6b6227e61ad97d991
Gaëtan RENAUDEAU <renaudeau.gaetan@gmail.com>
2010-11-08 10:32:47 +0100

    Improved GUI

--- userinterface_gui.c
+++ userinterface_gui.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <curses.h>
 
@@ -8,19 +9,24 @@
 
 #define STRBUF_SIZE 64
 
-#define PADDING_LEFT 2
-#define TITLE_TOP 0
-#define PENDU_TOP 2
-#define PENDU_HEIGHT 9
-#define WORD_TOP (PENDU_TOP+PENDU_HEIGHT)
-#define MESSAGE_TOP (WORD_TOP+1)
+#define MAINWIN_HEIGHT 20
+#define MAINWIN_WIDTH 50
+#define MAINWIN_PADDING_TOP ((LINES-MAINWIN_HEIGHT)/2)
+#define MAINWIN_PADDING_LEFT ((COLS-MAINWIN_WIDTH)/2)
 
-enum { MYCOLOR_DEFAULT = 1, MYCOLOR_TITLE, MYCOLOR_PENDU, MYCOLOR_PENDU_WARN, MYCOLOR_PENDU_LOST, MYCOLOR_LETTERS, MYCOLOR_INFO, MYCOLOR_ERROR };
+#define TITLE_TOP (MAINWIN_PADDING_TOP)
+#define PENDU_TOP (TITLE_TOP+2)
+#define PENDU_HEIGHT 9
+#define WORD_LEFT (MAINWIN_PADDING_LEFT+20)
+#define WORD_TOP (PENDU_TOP+3)
+#define LETTERS_TOP (WORD_TOP+2)
+#define MESSAGE_TOP (PENDU_TOP+PENDU_HEIGHT+1)
+
+enum { MYCOLOR_BACKGROUND, MYCOLOR_DEFAULT, MYCOLOR_TITLE, MYCOLOR_PENDU, MYCOLOR_PENDU_WARN,
+       MYCOLOR_PENDU_LOST, MYCOLOR_LETTERS, MYCOLOR_INFO, MYCOLOR_ERROR };
 
 /// GUI globals ///
 static WINDOW* mainwin; // fenetre principale
-static int mainwin_width;  // taille horizontale de la fenetre principale (char)
-static int mainwin_height; // taille verticale de la fenetre principale (char)
 
 static char strbuf[STRBUF_SIZE];
 
@@ -28,22 +34,21 @@ static void setColor(int color) {
   wattron(mainwin, COLOR_PAIR(color));
 }
 static void cleanLine(int line) {
-  mvwhline(mainwin, line, 0, ' ', mainwin_width);
+  mvwhline(mainwin, line, 0, ' ', COLS);
+}
+static void drawMessageAtLine(char* message, int line) {
+  cleanLine(line);
+  mvwprintw(mainwin, line, MAINWIN_PADDING_LEFT+(MAINWIN_WIDTH-strlen(message))/2, message);
 }
 static void drawTitle() {
   wattrset(mainwin, A_BOLD);
   setColor(MYCOLOR_TITLE);
-  mvwprintw(mainwin, TITLE_TOP, (mainwin_width-15)/2, "Le jeu du Pendu");
+  drawMessageAtLine("Le jeu du Pendu", TITLE_TOP);
   wattrset(mainwin, A_NORMAL);
 }
 static void drawPendu(int remainingTry) {
-  if(remainingTry>3)
-    setColor(MYCOLOR_PENDU);
-  else if(remainingTry==0)
-    setColor(MYCOLOR_PENDU_LOST);
-  else
-    setColor(MYCOLOR_PENDU_WARN);
-  int y = PENDU_TOP, x = (mainwin_width-8) / 2;
+  setColor(remainingTry>3 ? MYCOLOR_PENDU : (remainingTry==0 ? MYCOLOR_PENDU_LOST : MYCOLOR_PENDU_WARN));
+  int y = PENDU_TOP, x = MAINWIN_PADDING_LEFT;
   mvwprintw(mainwin, y++, x, (remainingTry<7 ? "+------ " : "        ") );
   mvwprintw(mainwin, y++, x, (remainingTry<6 ? "|/  |   " : (remainingTry<8 ? "|/      " : (remainingTry<9 ? "|       " : "        "))) );
   mvwprintw(mainwin, y++, x, (remainingTry<5 ? "|   o   " : (remainingTry<9 ? "|       " : "        ")) );
@@ -54,19 +59,17 @@ static void drawPendu(int remainingTry) {
   mvwprintw(mainwin, y++, x, (remainingTry<10 ? "========" : "        ") );
 }
 static void drawWord(char* word) {
-  wattrset(mainwin, A_BOLD);
   setColor(MYCOLOR_DEFAULT);
-  mvwprintw(mainwin, WORD_TOP, PADDING_LEFT, word);
+  mvwprintw(mainwin, WORD_TOP, WORD_LEFT, "   Word: ");
+  wattrset(mainwin, A_BOLD);
+  mvwprintw(mainwin, WORD_TOP, WORD_LEFT+9, word);
   wattrset(mainwin, A_NORMAL);
 }
 static void drawLetters(char* letters) {
+  setColor(MYCOLOR_DEFAULT);
+  mvwprintw(mainwin, LETTERS_TOP, WORD_LEFT, "Letters: ");
   setColor(MYCOLOR_LETTERS);
-  int len = strlen(letters);
-  mvwprintw(mainwin, WORD_TOP, mainwin_width-len-2, letters);
-}
-static void drawMessageAtLine(char* message, int line) {
-  cleanLine(line);
-  mvwprintw(mainwin, line, (mainwin_width-strlen(message))/2, message);
+  mvwprintw(mainwin, LETTERS_TOP, WORD_LEFT+9, letters);
 }
 static void drawMessage(char* message) {
   drawMessageAtLine(message, MESSAGE_TOP);
@@ -83,13 +86,12 @@ static void drawErrorMessage(char* message) {
 // Curses init
 extern void ui_init(Game* game) {
   mainwin = initscr();
-  mainwin_width = COLS;
-  mainwin_height = LINES;
   cbreak();
+  keypad(stdscr, TRUE);
   noecho();
   curs_set(0);
-  keypad(stdscr, TRUE);
   start_color();
+  init_pair(MYCOLOR_BACKGROUND, COLOR_BLACK, COLOR_WHITE);
   init_pair(MYCOLOR_DEFAULT, COLOR_WHITE, COLOR_BLACK);
   init_pair(MYCOLOR_TITLE, COLOR_MAGENTA, COLOR_BLACK);
   init_pair(MYCOLOR_PENDU, COLOR_GREEN, COLOR_BLACK);
@@ -132,6 +134,8 @@ extern void ui_onGameEnd(Game* game, int status) {
     drawInfoMessage("You lose !");
   else if(status==1)
     drawInfoMessage("You win !");
+  refresh();
+  sleep(1);
   setColor(MYCOLOR_DEFAULT);
   drawMessageAtLine("Press any key to quit.", MESSAGE_TOP+2);
   refresh();
