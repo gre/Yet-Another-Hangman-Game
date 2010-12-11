9e311f5daf8a08147feed804e1fc02064fafdb95
Gaëtan RENAUDEAU <renaudeau.gaetan@gmail.com>
2010-12-11 18:06:47 +0100

    Added licence&author blocs

--- dictionary.h
+++ dictionary.h
@@ -1,3 +1,8 @@
+/**
+ * Yet Another Hangman Game http://github.com/gre/Yet-Another-Hangman-Game
+ * @license GNU GPL version 3 http://www.gnu.org/licenses/gpl.html
+ * @author Gaetan Renaudeau <pro at grenlibre.fr>
+ */
 #ifndef _DICTIONARY_H
 #define _DICTIONARY_H
 
